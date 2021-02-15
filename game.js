6e58d8aa566efdda2d0692cb549faca1f1d1ef65
Steven <Mahalak.steve@outlook.com>
2021-02-14 19:21:20 -0500

    game is working/added opacity background

--- game.js
+++ game.js
@@ -1,52 +1,46 @@
 "use strict";
-// let test = document.querySelector(".test");
 
-// let startBtn = document.querySelector(".start-btn");
-// let ballPit = document.querySelector("ball-pit");
+const boy = document.querySelector(".boy");
+const rock = document.querySelector(".rock");
+const score = document.querySelector(".score");
 
-// startBtn.addEventListener("click", () => {
-//   let newDiv = document.createElement("div");
-//   newDiv.classList.add("ball");
+const jump = () => {
+  boy.classList.add("jump-animation");
+  setTimeout(() => boy.classList.remove("jump-animation"), 500);
+};
 
-//   test.append(newDiv);
-// });
+jump();
 
-// test.addEventListener("click", (e) => {
-//   if (e.target.classList.contains("ball")) {
-//     e.target.remove();
-//   }
-// });
+document.addEventListener("keypress", (event) => {
+  //if statement prevents user from starting another jump before animation is finished
+  if (!boy.classList.contains("jump-animation")) {
+    jump();
+  }
+});
 
-// const boy = document.querySelector(".boy");
-// const rock = document.querySelector(".rock");
-// const score = document.querySelector(".score");
+//============GAME Loop============================
 
-// function jump() {
-//   boy.classList.add("jump-animation");
-//   setTimeout(() => boy.classList.remove("jump-animation"), 500);
-// }
+setInterval(() => {
+  score.textContent++;
+  const boyTop = parseInt(window.getComputedStyle(boy).getPropertyValue("top"));
+  //can see how the top of the boy changes in pixels from the top
 
-// document.addEventListener("keypress", (event) => {
-//   if (!boy.classList.contains("jump-animation")) {
-//     jump();
-//   }
-// });
+  const rockLeft = parseInt(
+    window.getComputedStyle(rock).getPropertyValue("left")
+  );
+  //can see how the number of pixels that the rock moves left
 
-// setInterval(() => {
-//   const boyTop = parseInt(window.getComputedStyle(boy).getPropertyValue("top"));
-//   const rockLeft = parseInt(
-//     window.getComputedStyle(rock).getPropertyValue("left")
-//   );
-//   score.innerText++;
+  if (rockLeft < 0) {
+    rock.style.display = "none";
+  } else {
+    rock.style.display = "";
+    // this just removes the none value so you can see the rock again
+  }
 
-//   if (rockLeft < 0) {
-//     rock.style.display = "none";
-//   } else {
-//     rock.style.display = "";
-//   }
-
-//   if (rockLeft < 50 && rockLeft > 0 && boyTop > 150) {
-//     alert("You got a score of: " + score.innerText + "\n\nPlay again?");
-//     location.reload();
-//   }
-// }, 50);
+  if (rockLeft < 50 && rockLeft > 0 && boyTop > 150) {
+    alert(
+      "Game Over! You got a score of: " + score.textContent + "\n\nPlay again?"
+    );
+    location.reload();
+  }
+}, 50); // we are updating the state of the game every 50 milli seconds
